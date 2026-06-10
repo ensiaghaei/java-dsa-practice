f56c6476191877e84f7442a8312218918f47d882
ensiaghaei <153228840+ensiaghaei@users.noreply.github.com>
2026-06-09 19:50:51 -0700

    add ValidAnagram

---
+++ src/hashmap/ValidAnagram.java
@@ -0,0 +1,41 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+
+            if (!map.containsKey(c)) {
+                return false;
+            }
+
+            map.put(c, map.get(c) - 1);
+
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+}
